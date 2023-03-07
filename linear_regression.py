5e2bfb738073ea8cc322c96ca8ed7b69656f99e3
Ilias Gimranov <ilyas_gi@mail.ru>
2023-03-07 12:41:01 +0600

    Refactor linear regression experiment

--- linear_regression.py
+++ linear_regression.py
@@ -2,17 +2,15 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
 from cost_function import CostFunction
-from gradient_descent import GradientDescent
 from gradient_descent_experiment import GradientDescentExperiment
 
 
 class LinearRegressionExperiment(GradientDescentExperiment):
     def setup_func(self):
-        self.real_theta_0 = 3*(np.random.rand()-0.5)
-        self.real_theta_1 = 5*(np.random.rand()-0.5)
+        self.real_theta = [3*(np.random.rand()-0.5), 5*(np.random.rand()-0.5)]
         self.x = np.arange(-10, 10, 0.5)
         self.y = list(map(lambda u: u - 2*(np.random.rand()-0.5),
-                          list(self.real_theta_0+self.real_theta_1*self.x))
+                          list(self.real_theta[0]+self.real_theta[1]*self.x))
                       )
         return CostFunction(list(map(lambda val: [1, val], self.x)), self.y)
 
@@ -20,15 +18,15 @@ class LinearRegressionExperiment(GradientDescentExperiment):
         iterations = self.get_history_iterations()
         values = self.get_history_values()
         c_v = np.frompyfunc(self.get_func(), 2, 1)
-        X = np.arange(-10, 10, 0.1)
-        Y = np.arange(-10, 10, 0.1)
+        Y = np.arange(-15, 15, 0.1)
+        X = np.arange(-80, 80, 0.5)
         X, Y = np.meshgrid(X, Y)
         Z = c_v(X, Y)
 
         print('result theta')
         print(self.theta)
         print('real theta')
-        print(self.real_theta_0, self.real_theta_1)
+        print(self.real_theta)
 
         fig, ax = plt.subplots()
         ax.plot(self.x, self.y, 'b.')
@@ -46,25 +44,19 @@ class LinearRegressionExperiment(GradientDescentExperiment):
         ax.plot(x2, y2)
         plt.show()
 
-        # Plot the surface
         fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
         ax.view_init(40, -10)
         ax.plot_surface(X, Y, Z, cmap=cm.Blues)
         ax.plot3D(list(map(lambda values: values[0], values)), list(map(
             lambda values: values[1], values)), list(map(lambda v: c_v(*v), values)), 'g.')
-
         plt.show()
 
         fig, ax = plt.subplots()
         ax.plot(list(range(0, len(iterations))), iterations, 'b.')
         plt.show()
 
-        Y = np.arange(-15, 15, 0.1)
-        X = np.arange(-80, 80, 0.5)
-        X, Y = np.meshgrid(X, Y)
-        Z = np.array(c_v(X, Y).tolist())
         fig, ax = plt.subplots()
-        ax.contour(X, Y, Z, levels=np.arange(-200, 3000, 150))
+        ax.contour(X, Y, np.array(Z.tolist()), levels=np.arange(-200, 3000, 150))
         ax.plot(list(map(lambda values: values[0], values)),
                 list(map(lambda values: values[1], values)), 'g.')
         ax.plot([self.theta[0]], [self.theta[1]], 'r.')
@@ -72,7 +64,7 @@ class LinearRegressionExperiment(GradientDescentExperiment):
 
 
 lre = LinearRegressionExperiment()
-lre.set_start_theta([-8, 10])
+lre.set_start_theta([-15, 15])
 lre.set_max_iterations(10000)
 
 rates = [0.001, 0.01, 0.05]
