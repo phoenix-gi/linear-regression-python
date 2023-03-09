948e342a8f6496a777996d5bffd68c2a494b7a87
Ilias Gimranov <ilyas_gi@mail.ru>
2023-03-09 11:14:56 +0600

    Add gradient descent experiemnt with parabaloid

---
+++ parabaloid_function.py
@@ -0,0 +1,19 @@
+from base_function import BaseFunction
+
+
+class ParabaloidFunction(BaseFunction):
+    def __init__(self, a=1, b=1, sx=0, sy=0, sz=0) -> None:
+        self.a = a
+        self.b = b
+        self.sx = sx
+        self.sy = sy
+        self.sz = sz
+
+    def __call__(self, *theta):
+        return self.sz+self.a * ((theta[0]+self.sx)**2)+self.b * ((theta[1]+self.sy)**2)
+
+    def derivative(self, partial_index):
+        if partial_index == 0:
+            return lambda *theta: 2*(theta[0]+self.sx)*self.a
+        if partial_index == 1:
+            return lambda *theta: 2*(theta[1]+self.sy)*self.b
