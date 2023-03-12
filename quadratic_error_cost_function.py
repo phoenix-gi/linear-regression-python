1fa5ca8c255a6c91f4821ae2d582f2a612b1c2e1
Ilias Gimranov <ilyas_gi@mail.ru>
2023-03-12 12:20:14 +0600

    Move hypothesis function of quadratic error cost function to separate method

--- quadratic_error_cost_function.py
+++ quadratic_error_cost_function.py
@@ -9,13 +9,16 @@ class QuadraticErrorCostFunction(BaseFunction):
     def __call__(self, *theta):
         return sum(
             map(lambda i:
-                (sum(map(lambda j: theta[j]*self.x[i][j], range(0, len(theta)))) - self.y[i]) ** 2, range(0, len(self.x)))
+                (self.h(theta, self.x[i]) - self.y[i]) ** 2, range(0, len(self.x)))
         )/(2.0 * len(self.x))
+
+    def h(self, theta, x_i):
+        return sum(map(lambda j: theta[j]*x_i[j], range(0, len(theta))))
 
     def derivative(self, partial_index):
         return lambda *theta: (
             sum(
                 map(lambda i:
-                    self.x[i][partial_index]*(sum(map(lambda j: theta[j]*self.x[i][j], range(0, len(theta)))) - self.y[i]), range(0, len(self.x)))
+                    self.x[i][partial_index]*(self.h(theta, self.x[i]) - self.y[i]), range(0, len(self.x)))
             )/len(self.x)
         )
